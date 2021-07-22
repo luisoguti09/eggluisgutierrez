873c873a9fe31a3a46690a1deaf60bc4bad50ec9
Luis <luisoguti09@gmail.com>
2021-07-21 21:11:29 -0300

    Esta clase es para ayudar y simplificar

--- src/com/egg/lgutierrez/guiapoo/Ejercicio1.java
+++ src/com/egg/lgutierrez/guiapoo/Ejercicio1.java
@@ -12,5 +12,14 @@ package com.egg.lgutierrez.guiapoo;
  * @author Luis
  */
 public class Ejercicio1 {
-    
+    public static void main(String[] args) {
+         boolean par = false;
+         int num = 2;
+         
+         if (num % 2 == 0){
+             System.out.println("Es par");
+         }else {
+             System.out.println("es impar");
+       }
+    }
 }
