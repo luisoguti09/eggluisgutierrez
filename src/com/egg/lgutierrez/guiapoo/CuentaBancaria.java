a3c6313978339b1c4685935703001d4b664afe6e
Luis <luisoguti09@gmail.com>
2021-07-29 18:09:23 -0300

    Esta clase es para ayudar y simplificar

--- src/com/egg/lgutierrez/guiapoo/CuentaBancaria.java
+++ src/com/egg/lgutierrez/guiapoo/CuentaBancaria.java
@@ -18,11 +18,89 @@ que el usuario no saque más del 20%.
  */
 package com.egg.lgutierrez.guiapoo;
 
+import com.egg.lgutierrez.servicios.ServicioCuentaBancaria;
+import com.egg.lgutierrez.utils.MathUtils;
+
 /**
  *
  * @author Luis
  */
 public class CuentaBancaria {
     private long dni;
+    private int numeroCuenta;
+    private double interes,saldoActual;    
+    
+    public CuentaBancaria() {
+    }
+
+    public CuentaBancaria(long dni, int numeroCuenta, double saldoActual) {
+        this.dni = dni;
+        this.numeroCuenta = numeroCuenta;
+        this.saldoActual = saldoActual;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+    
+    public double getInteres() {
+        return interes;
+    }
+
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+    
+    public void crearCuenta(){
+        System.out.println("Bienvenido a la Banca Gando a la gente!");
+        System.out.println("Por favor ingrese su DNI");
+        this.setDni(MathUtils.pedirNumero());
+        System.out.println("Por favor ingrese su Número de Cuenta");
+        this.setNumeroCuenta(MathUtils.pedirNumero());
+        System.out.println("Por favor ingrese su cantidad de dinero");
+        this.setSaldoActual(MathUtils.pedirNumero());        
+    }
+    public void mostrarCuenta(){
+        System.out.println("Su DNI es: " + this.getDni());
+        System.out.println("Su número de cuenta es: " + this.getNumeroCuenta());
+        System.out.println("Su saldo Actual es: " + this.getSaldoActual());
+    }
+    
+    public static void main(String[] args) {
+        CuentaBancaria cta = new CuentaBancaria();
+        cta.crearCuenta();
+        cta.setSaldoActual(ServicioCuentaBancaria.ingresaDinero(cta.getSaldoActual()));
+        System.out.println(cta.getSaldoActual());
+        cta.setSaldoActual(ServicioCuentaBancaria.retiraDinero(cta.getSaldoActual()));
+        System.out.println(cta.getSaldoActual());
+        System.out.println("Segun su dinero ingresado el Interés a pagar es: " + ServicioCuentaBancaria.tasaInteres(cta.getSaldoActual()));
+        cta.setSaldoActual(ServicioCuentaBancaria.generarInteres(cta.getSaldoActual()));
+        System.out.println(cta.getSaldoActual());
+        cta.setSaldoActual(ServicioCuentaBancaria.extraccionRapida(cta.getSaldoActual()));
+        System.out.println(cta.getSaldoActual());
+        System.out.println("Los datos de tu cuenta son los siguientes");
+        cta.mostrarCuenta();
+        
+    }
     
 }
--- src/com/egg/lgutierrez/servicios/ServicioCuentaBancaria.java
+++ src/com/egg/lgutierrez/servicios/ServicioCuentaBancaria.java
@@ -1,9 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * • Método ingresar(double ingreso): el método recibe una cantidad de dinero a
+ingresar y se la sumara a saldo actual.
+• Método retirar(double retiro): el método recibe una cantidad de dinero a retirar y
+se la restará al saldo actual. Si la cuenta no tiene la cantidad de dinero a retirar, se
+pondrá el saldo actual en 0.
+• Método extraccionRapida(): le permitirá sacar solo un 20% de su saldo. Validar
+que el usuario no saque más del 20%.
+• Método consultarSaldo(): permitirá consultar el saldo disponible en la cuenta.
+• Método consultarDatos(): permitirá mostrar todos los datos de la cuenta
  */
 package com.egg.lgutierrez.servicios;
+
+import com.egg.lgutierrez.utils.MathUtils;
 
 /**
  *
@@ -11,4 +19,70 @@ package com.egg.lgutierrez.servicios;
  */
 public class ServicioCuentaBancaria {
     
+    
+    public static double ingresaDinero(double saldo){
+       double ingreso;
+       System.out.println("Cuánto dinero quiere ingresar?");
+        ingreso = MathUtils.pedirNumero();
+        ingreso = ingreso + saldo;
+        System.out.println("Su Saldo actual es:");
+        return ingreso;
+    }
+    
+    public static double retiraDinero(double saldo){
+        double retiro;
+        System.out.println("Cuánto Dinero Quiere retirar");
+        retiro = MathUtils.pedirNumero();
+        if (retiro <= saldo){
+            saldo = saldo - retiro ;
+        }else {
+            saldo = 0;
+            System.out.println("Ese importe supera al de su cuenta! Por vivo su saldo actual es $0");
+        }
+        return saldo;
+    }
+    
+    public static double tasaInteres(double saldo){
+        if(saldo >= 1000 && saldo <= 1000000){
+            return  1.37f;
+        }
+        if(saldo <= 1000001){
+            return 1.34f;
+        }
+        if (saldo >= 999){
+            System.out.println("Volvé cuando tengas más plata");
+        } 
+        return 0f;
+    }
+    
+    public static double generarInteres(double saldo){
+        if(saldo >= 1000 && saldo <= 1000000){
+            return (saldo * 1.37f);
+        }
+        if(saldo >= 1000001){
+            return (saldo * 1.34f);
+        }
+        if (saldo >= 999){
+            System.out.println("Volvé cuando tengas más plata");
+        } 
+        return 0f;
+    }
+    
+    public static double extraccionRapida(double saldo){
+        double retiro, valorMaximo;
+        
+        System.out.println("Cuanto dinero Express quiere retirar?");
+        retiro = MathUtils.pedirNumero();
+        valorMaximo = (20 * saldo) / 100; 
+        if (retiro <= valorMaximo){
+           return saldo - retiro;
+        }else{
+            System.out.println("No se permite retirar mas del 20% en este método");
+        }
+        return saldo;
+    }
+    
+    
+    
+   
 }
