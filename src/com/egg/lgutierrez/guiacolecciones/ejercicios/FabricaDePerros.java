4d2e579e3e76af167c4662901c5efd313db9ab85
Luis <luisoguti09@gmail.com>
2021-08-10 19:46:41 -0300

    Esta clase es para ayudar y simplificar

--- src/com/egg/lgutierrez/guiacolecciones/ejercicios/FabricaDePerros.java
+++ src/com/egg/lgutierrez/guiacolecciones/ejercicios/FabricaDePerros.java
@@ -7,6 +7,9 @@ quiere salir. Si decide salir, se mostrará todos los perros guardados en el Arr
  */
 package com.egg.lgutierrez.guiacolecciones.ejercicios;
 
+import com.egg.lgutierrez.guiacolecciones.servicios.ServicioFabricaDePerros;
+import java.util.ArrayList;
+
 /**
  *
  * @author Luis
@@ -14,5 +17,43 @@ package com.egg.lgutierrez.guiacolecciones.ejercicios;
 public class FabricaDePerros {
     
     private String raza, nombre;
-    private ArrayList <ServicioMascota> mscotas;
+    private ArrayList <ServicioFabricaDePerros> mascotas;
+
+    public FabricaDePerros(String raza, String nombre, ArrayList<ServicioFabricaDePerros> mascotas) {
+        this.raza = raza;
+        this.nombre = nombre;
+        this.mascotas = mascotas;
+    }
+
+    public FabricaDePerros() {
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<ServicioFabricaDePerros> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(ArrayList<ServicioFabricaDePerros> mascotas) {
+        this.mascotas = mascotas;
+    }
+    public static void main(String[] args) {
+        FabricaDePerros fab = new FabricaDePerros();
+        fab.setRaza(ServicioFabricaDePerros.crearRaza());
+        
+    }
 }
--- src/com/egg/lgutierrez/guiacolecciones/servicios/ServicioFabricaDePerros.java
+++ src/com/egg/lgutierrez/guiacolecciones/servicios/ServicioFabricaDePerros.java
@@ -5,10 +5,40 @@
  */
 package com.egg.lgutierrez.guiacolecciones.servicios;
 
+import com.egg.lgutierrez.utils.StringUtils;
+import java.util.ArrayList;
+
 /**
  *
  * @author Luis
  */
 public class ServicioFabricaDePerros {
     
+    public static String crearRaza() {
+        boolean salir = false;
+        String respuesta;
+            System.out.println("Escriba la raza de su mascota");
+            String raza = StringUtils.pedirRespuestaString();
+            System.out.println("Escriba el nombre de su mascota");
+            String nombre = StringUtils.pedirRespuestaString();
+            String mascota = raza + " " + nombre;
+
+        return mascota;   
+    }
+    
+    public static void mostrarPesho(){
+        
+    }
+           
+    public static void mostrarRaza (String raza, String nombre){
+        boolean salir = false;
+        String respuesta;
+        do{
+            System.out.println("Desea agregar otra mascota?");
+        System.out.println("Usted ha ingresado los siguientes datos: ");
+        System.out.println("Raza: " + raza);
+        System.out.println("Nombre: " + nombre);
+        }while(salir);
+    }
+    }
 }
